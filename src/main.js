23d2400fbe05f0c7db9b9e633c457d5bdcbfcef2
Your yunchao <you@example.com>
2020-03-04 16:41:20 +0800

    vant 组件库-rem 适配

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//  所有import 都在上面
+// vant 导入
+import Vant from 'vant'
+// vant 样式导入
+import 'vant/lib/index.css'
+// res基准值
+import 'amfe-flexible/index.min.js'
+
+// 全局注册
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
