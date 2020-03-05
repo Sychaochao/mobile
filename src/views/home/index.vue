87a6206bc44765134f6ebd53e4ed9c84a91d486a
Your yunchao <you@example.com>
2020-03-05 21:50:20 +0800

    文章上拉效果--瀑布流

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,78 @@
 <template>
-  <div class='container'>Home</div>
+  <div class="container">
+    <van-tabs v-model="activeChannelIndex">
+      <!-- <van-tab title="标签名称">当前标签对应的内容</van-tab> -->
+      <van-tab title="推荐">
+
+        <div class="scroll-wrapper">
+          <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+            <van-cell v-for="item in list" :key="item" :title="item" />
+          </van-list>
+        </div>
+      </van-tab>
+      <van-tab title="数据库">数据库内容展示</van-tab>
+      <van-tab title="后端">后端内容展示</van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'home-index'
+  name: 'home-index',
+  data () {
+    return {
+      // 瀑布流
+      list: [], // 接受加载好的内容
+      loading: false,
+      finished: false,
+      // 激活 频道下标标志
+      activeChannelIndex: 1
+    }
+  },
+  methods: {
+    // 瀑布流加载方法
+    onLoad () {
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false // 清除
+
+        // 数据 全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true // 停止瀑布流加载
+        }
+      }, 1000)
+    }
+  }
 }
 </script>
 
 <style scoped lang='less'>
+.van-tabs {
+  // 弹性布局
+  display: flex;
+  // 灵活的项目将垂直显示，正如一个列一样
+  flex-direction: column;
+  height: 100%;
+  /deep/ .van-tabs__content {
+    // flex:1;的值是1 1 0%，【父控件有剩余空间占1份放大，父控件空间不足按1缩小，自身的空间大小是0%】
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane {
+    height: 100%;
+    // 给上拉列表设置样式
+    .scroll-wrapper {
+      height: 100%;
+      // 瀑布流 区域 如果垂直方向内容过多 要呈现滚动条
+      overflow-y: auto;
+    }
+  }
+  // 给频道下边沿横向设置样式
+  /deep/ .van-tabs__line {
+    background-color: rgb(30, 219, 210);
+  }
+}
 </style>
